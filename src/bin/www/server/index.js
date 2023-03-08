da7078a6bd89b95f26e28adf9288584cdb63c17b
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-08 01:48:40 -0400

    feat: add log when server is closed

--- src/bin/www/server/index.js
+++ src/bin/www/server/index.js
@@ -6,12 +6,15 @@ const server = http.createServer(app);
 const initServer = () => {
   const port = 3000;
   const initialCallback = () => {
-    console.log(`API listening at port: ${port}`);
+    console.log(`API server listening at port: ${port}`);
   }
   const serverRef = server.listen(port, initialCallback);
   return serverRef;
 }
 
-const stopServer = (args) => server.close(args);
+const stopServer = (args) => {
+  server.close(args);
+  console.log('API server closed');
+}
 
 module.exports = { initServer, stopServer };
