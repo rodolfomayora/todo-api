4757ffc451a22bc4fa1ec9462e67299bf87e2e20
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-08 01:43:36 -0400

    test: add integration tests for api server

---
+++ src/bin/www/server/index.integration.test.js
@@ -0,0 +1,32 @@
+const supertest = require('supertest');
+const server = require('../server');
+
+const setup = () => {
+  const serverRef = server.initServer();
+  const request = supertest(serverRef);
+  return { request };
+}
+
+afterEach((done) => {
+  server.stopServer(done);
+});
+
+describe('server', () => {
+  test('When recieves GET: /api/v1 request, then responses with welcome message', async () => {
+    const { request } = setup();
+    const response = await request.get('/api/v1');
+    const { statusCode, headers, body } = response;
+    expect(statusCode).toBe(200);
+    expect(headers['content-type']).toContain('application/json');
+    expect(body).toEqual({ message: 'TODO RESTful API' });
+  });
+
+  test('When recieves request to unknown route, then responses with error message', async () => {
+    const { request } = setup();
+    const response = await request.get('/');
+    const { statusCode, headers, body } = response;
+    expect(statusCode).toBe(404);
+    expect(headers['content-type']).toContain('application/json');
+    expect(body).toEqual({ message: 'The resquested resource was not found on this server' });
+  });
+});
