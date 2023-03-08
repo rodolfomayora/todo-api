4875caa9f344703896c3aaa601c1bb4ceefbea49
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-08 01:50:21 -0400

    feat: add new api endpoint for unknown routes

--- src/app.js
+++ src/app.js
@@ -5,5 +5,10 @@ app.use(express.json()); // to allow JSON entries
 app.get('/api/v1', (reques, response) => {
   return response.status(200).json({ message: 'TODO RESTful API'});
 })
+app.use((request, response) => {
+  return response
+    .status(404)
+    .json({ message: 'The resquested resource was not found on this server' });
+})
 
 module.exports = app;
