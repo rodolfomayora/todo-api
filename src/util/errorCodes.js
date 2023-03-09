d6d25a57e0d861fdcf7d6cdf5b05b3527ed76d87
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-09 18:54:51 -0400

    feat: add unauthroized error code

--- src/util/errorCodes.js
+++ src/util/errorCodes.js
@@ -1,4 +1,5 @@
 const handledError = Object.freeze({ // freeze to avoid key/value mutations
+  UNAUTHORIZED: 'UNAUTHORIZED', // Authentication failed or user does not have permissions
   UNKNOWN_ROUTE: 'UNKNOWN_ROUTE',
   BAD_REQUEST: 'BAD_REQUEST',
   NOT_FOUND: 'NOT_FOUND'
