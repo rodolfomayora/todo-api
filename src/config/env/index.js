6bb7e73163651e7631850a11df6cea519e7c6f25
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-09 17:14:00 -0400

    chore: move env handler file to

---
+++ src/config/env/index.js
@@ -0,0 +1,20 @@
+const ports = {
+  default: 3000,
+  production: 3000,
+  development: 3002,
+  testing: 3003
+}
+
+const ENV = process.env.NODE_ENV;
+const IS_PRODUCTION  = ENV === 'production';
+const IS_DEVELOPMENT = ENV === 'development';
+const IS_TESTING     = ENV === 'testing';
+const PORT = ports[ENV] ?? ports.default;
+
+module.exports = {
+  ENV,
+  IS_PRODUCTION,
+  IS_DEVELOPMENT,
+  IS_TESTING,
+  PORT,
+};
