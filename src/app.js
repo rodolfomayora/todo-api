6133f89d598c87f2628f0f1a91156d0c16361e76
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-09 19:24:13 -0400

    feat: implemente error response middlware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const apiMiddeware = require('./middleware/api');
+const errorMiddleware = require('./middleware/error');
+const errorCodes = require('./util/errorCodes');
+
 const app = express();
 
 app.use(express.json()); // to allow JSON entries
@@ -8,10 +11,14 @@ app.use(apiMiddeware.requestLogger);
 app.get('/api/v1', (reques, response) => {
   return response.status(200).json({ message: 'TODO RESTful API'});
 })
-app.use((request, response) => {
-  return response
-    .status(404)
-    .json({ message: 'The resquested resource was not found on this server' });
-})
+
+app.use((request, response, next) => {
+  const error = new Error('The resquested resource was not found on this server');
+  error.code = errorCodes.UNKNOWN_ROUTE;
+  next(error);
+});
+
+// error middlewares
+app.use(errorMiddleware.errorResponse);
 
 module.exports = app;
