ad14cf1c7881490169c20d4a484f06c5332871fd
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-09 18:01:41 -0400

    feat: implement request logger middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const apiMiddeware = require('./middleware/api');
 const app = express();
 
 app.use(express.json()); // to allow JSON entries
+app.use(apiMiddeware.requestLogger);
+
 app.get('/api/v1', (reques, response) => {
   return response.status(200).json({ message: 'TODO RESTful API'});
 })
