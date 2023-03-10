9cb4ddac8b7da9e464d52d3f1deff912950e6bf9
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-09 21:58:56 -0400

    refacter: implement unknown route middlware

--- src/app.js
+++ src/app.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const apiMiddeware = require('./middleware/api');
 const errorMiddleware = require('./middleware/error');
-const errorCodes = require('./util/errorCodes');
 
 const app = express();
 
 app.use(express.json()); // to allow JSON entries
 app.use(apiMiddeware.requestLogger);
-
 app.get('/api/v1', (reques, response) => {
   return response.status(200).json({ message: 'TODO RESTful API'});
 })
-
-app.use((request, response, next) => {
-  const error = new Error('The resquested resource was not found on this server');
-  error.code = errorCodes.UNKNOWN_ROUTE;
-  next(error);
-});
+app.use(apiMiddeware.unknownRoute);
 
 // error middlewares
 app.use(errorMiddleware.errorResponse);
