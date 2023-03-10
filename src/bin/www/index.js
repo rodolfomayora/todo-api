318dcc7a6ad8ccd050d1a568b506d040cd35ca98
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-10 17:06:41 -0400

    feat: implement mongoDB configuration on www file

--- src/bin/www/index.js
+++ src/bin/www/index.js
@@ -1,1 +1,24 @@
-require('../../config/httpServer').startServer();
+const httpServer = require('../../config/httpServer');
+const mongoDB = require('../../config/mongoDB');
+
+const init = async () => {
+  // 1) server config
+  httpServer.startServer();
+
+  // 2) data base config
+  await mongoDB.openConnection();
+
+  process.on('SIGINT', async () => { // for development
+    await mongoDB.closeConnection();
+    httpServer.stopServer();
+    process.exit(0);
+  });
+
+  process.on('SIGTERM', async () => { // for production
+    await mongoDB.closeConnection();
+    httpServer.stopServer();
+    process.exit(0);
+  });
+}
+
+init();
