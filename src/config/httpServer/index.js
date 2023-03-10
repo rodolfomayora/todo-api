9824b6ccc2581b83a4ad69302cd93c2ccd11b887
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-10 16:50:26 -0400

    refactor: change http server instance reference

--- src/config/httpServer/index.js
+++ src/config/httpServer/index.js
@@ -3,19 +3,21 @@ const app = require('../../app');
 const env = require('../env');
 const logger = require('../../util/logger');
 
-const server = http.createServer(app);
+let serverRef = null;
 
 const startServer = () => {
+  const server = http.createServer(app);
   const port = env.PORT;
   const handleListen = () => {
     return logger.info(`API http server listening at port: ${port}`);
   }
-  const serverRef = server.listen(port, handleListen);
+  serverRef = server.listen(port, handleListen);
   return serverRef;
 }
 
 const stopServer = (args) => {
-  server.close(args);
+  serverRef.close(args);
+  serverRef = null;
   logger.info('API http server closed');
 }
 
