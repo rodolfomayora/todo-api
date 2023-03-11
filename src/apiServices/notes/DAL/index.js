940c858d0d34877e059650a675014cdda1c73375
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-11 17:43:25 -0400

    feat: add DAL layer for GET /notes/:noteId

--- src/apiServices/notes/DAL/index.js
+++ src/apiServices/notes/DAL/index.js
@@ -1,27 +1,51 @@
-// const errorCodes = require('../../../util/errorCodes');
+const mongoose = require('mongoose');
+const errorCodes = require('../../../util/errorCodes');
 const Note = require('./model/Note');
+
+const { CastError } = mongoose.Error;
 
 const create = async (noteData) => {
   const { content } = noteData;
   const newNote = { content };
-  const response = await Note.create(newNote);
-  const rawData = response.toObject();
-  return rawData;
+  const document = await Note.create(newNote);
+  const rawData = document.toObject();
+  return { ...rawData };
 }
 
 const readAll = async (query) => {
   const { limit } = query;
   const defultLimit = 10;
-  const rawNotes = await Note
+  const rawDataList = await Note
     .find()
     .sort({ createdAt: 'descending' })
     .limit(limit ?? defultLimit)
     .lean();
 
-  return rawNotes;
+  return [...rawDataList];
 }
 
-// const readById = async (noteId) => {}
+const readById = async (noteId) => {
+  try {
+    const document = await Note.findById(noteId).lean();
+    const isNotFound = !document;
+    if (isNotFound) {
+      const handledError = new Error('Not Found, note ID not match');
+      handledError.code = errorCodes.NOT_FOUND;
+      throw handledError;
+    }
+    return { ...document };
+
+  } catch (error) {
+    const isCastError = error instanceof CastError;
+    if (isCastError) {
+      const handledError = new Error('Not valid noteId value');
+      handledError.code = errorCodes.BAD_REQUEST;
+      handledError.stack = error.stack;
+      throw handledError;
+    }
+    throw error;
+  }
+}
 
 // const updateById = async (noteId, body) => {}
 
@@ -30,4 +54,5 @@ const readAll = async (query) => {
 module.exports = {
   create,
   readAll,
+  readById,
 }
