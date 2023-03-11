edb864208579d358813f07063af8979c54f4aab9
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-11 01:17:04 -0400

    test: add integration test for mongoose connection middleware

--- src/config/httpServer/index.integration.test.js
+++ src/config/httpServer/index.integration.test.js
@@ -2,11 +2,16 @@ const supertest = require('supertest');
 const mongoMemory = require('../mongoMemory');
 const httpServer = require('.');
 
-const setup = async () => {
+const setup = () => {
   const serverRef = httpServer.startServer();
-  await mongoMemory.openConnection();
   const request = supertest(serverRef);
   return { request };
+}
+
+const setupWithMongo = async () => {
+  const config = setup();
+  await mongoMemory.openConnection();
+  return { ...config };
 }
 
 afterEach(async () => {
@@ -16,7 +21,7 @@ afterEach(async () => {
 
 describe('API http server', () => {
   test('When recieves GET: /api/v1 request, then responses with welcome message', async () => {
-    const { request } = await setup();
+    const { request } = await setupWithMongo();
     const response = await request.get('/api/v1');
     const { statusCode, headers, body } = response;
     expect(statusCode).toBe(200);
@@ -25,7 +30,7 @@ describe('API http server', () => {
   });
 
   test('When recieves request to unknown route, then responses with 404 server error message', async () => {
-    const { request } = await setup();
+    const { request } = await setupWithMongo();
     const response = await request.get('/random');
     const { statusCode, headers, body } = response;
     expect(statusCode).toBe(404);
@@ -33,7 +38,12 @@ describe('API http server', () => {
     expect(body).toEqual({ message: 'The resquested resource was not found on this server' });
   });
 
-  test.todo('When recieves request and database is not connected, then responses with 503 server error message');
-
-  test.todo('When occurs an unknown error, then responses with 500 server error message');
+  test('When recieves request and database is not connected, then responses with 503 server error message', async () => {
+    const { request } = setup();
+    const response = await request.get('/api/v1');
+    const { statusCode, headers, body } = response;
+    expect(statusCode).toBe(503);
+    expect(headers['content-type']).toContain('application/json');
+    expect(body).toEqual({ message: 'MongoDB not connected yet' });
+  });
 });
