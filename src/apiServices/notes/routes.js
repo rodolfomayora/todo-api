05aecf0a2a25c0d4527a04726c269cd3c12492ae
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-11 15:13:23 -0400

    feat: add route for GET /notes

--- src/apiServices/notes/routes.js
+++ src/apiServices/notes/routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const notesController = require('./controller');
 
 router.post('/', notesController.create);
+router.get('/', notesController.readAll);
 
 module.exports = router;
