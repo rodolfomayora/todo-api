478f0c13178c6873c254dedb2857d4fdc38318bf
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-11 19:46:33 -0400

    feat: add route for DELETE /notes/:noteId

--- src/apiServices/notes/routes.js
+++ src/apiServices/notes/routes.js
@@ -4,5 +4,6 @@ const notesController = require('./controller');
 router.post('/', notesController.create);
 router.get('/', notesController.readAll);
 router.get('/:noteId', notesController.readById);
+router.delete('/:noteId', notesController.deleteById);
 
 module.exports = router;
