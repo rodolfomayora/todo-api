cb517cc357054d9d8337707c5e7d68ea6d7625d2
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-11 21:07:00 -0400

    feat: add route for PATCH /notes/:noteId

--- src/apiServices/notes/routes.js
+++ src/apiServices/notes/routes.js
@@ -4,6 +4,7 @@ const notesController = require('./controller');
 router.post('/', notesController.create);
 router.get('/', notesController.readAll);
 router.get('/:noteId', notesController.readById);
+router.patch('/:noteId', notesController.updateById);
 router.delete('/:noteId', notesController.deleteById);
 
 module.exports = router;
