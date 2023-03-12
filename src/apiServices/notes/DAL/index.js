095bc428eae099a9600fa4e8d90e3a6f48133b48
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-11 21:03:29 -0400

    feat: add DAL layer for PATCH /notes:noteId

--- src/apiServices/notes/DAL/index.js
+++ src/apiServices/notes/DAL/index.js
@@ -7,28 +7,28 @@ const { CastError } = mongoose.Error;
 const create = async (noteData) => {
   const { content } = noteData;
   const newNote = { content };
-  const document = await Note.create(newNote);
-  const rawData = document.toObject();
-  return { ...rawData };
+  const response = await Note.create(newNote);
+  const document = response.toObject();
+  return { ...document };
 }
 
 const readAll = async (query) => {
   const { limit } = query;
   const defultLimit = 10;
-  const rawDataList = await Note
+  const documents = await Note
     .find()
     .sort({ createdAt: 'descending' })
     .limit(limit ?? defultLimit)
     .lean();
 
-  return [...rawDataList];
+  return [...documents];
 }
 
 const readById = async (noteId) => {
   try {
     const document = await Note.findById(noteId).lean();
-    const isNotFound = !document;
-    if (isNotFound) {
+    const documentNotFound = !document;
+    if (documentNotFound) {
       const handledError = new Error('Not Found, note ID not match');
       handledError.code = errorCodes.NOT_FOUND;
       throw handledError;
@@ -47,13 +47,39 @@ const readById = async (noteId) => {
   }
 }
 
-// const updateById = async (noteId, body) => {}
+const updateById = async (noteId, noteData) => {
+  const updatedData = { ...noteData }
+  const config = { new: true }; // return updated document
+  try {
+    const document = await Note
+      .findByIdAndUpdate(noteId, updatedData, config)
+      .lean();
+
+    const documentNotFound = !document;
+    if (documentNotFound) {
+      const handledError = new Error('Not Found, note ID not match');
+      handledError.code = errorCodes.NOT_FOUND;
+      throw handledError;
+    }
+    return { ...document };
+
+  } catch (error) {
+    const isCastError = error instanceof CastError;
+    if (isCastError) {
+      const handledError = new Error('Not valid note ID value');
+      handledError.code = errorCodes.BAD_REQUEST;
+      handledError.stack = error.stack;
+      throw handledError;
+    }
+    throw error;
+  }
+}
 
 const deleteById = async (noteId) => {
   try {
     const document = await Note.findByIdAndDelete(noteId);
-    const isNotFound = !document;
-    if (isNotFound) {
+    const documentNotFound = !document;
+    if (documentNotFound) {
       const handledError = new Error('Not Found, note ID not match');
       handledError.code = errorCodes.NOT_FOUND;
       throw handledError;
@@ -76,5 +102,6 @@ module.exports = {
   create,
   readAll,
   readById,
+  updateById,
   deleteById
 }
