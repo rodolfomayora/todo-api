ed91da5be930852ccf787e8d91517090f5bf3e5d
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-11 21:10:45 -0400

    fix: change url-encoded to true

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(apiMiddeware.requestLogger);
 app.use(apiMiddeware.mongooseConnection);
 app.use('/api/v1', routers);
