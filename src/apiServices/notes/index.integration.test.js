95a46c8fb3a382c96ee60e3bcf5cb0de902de3bf
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-13 04:16:28 -0400

    test: add integration test forPOST /notes

--- src/apiServices/notes/index.integration.test.js
+++ src/apiServices/notes/index.integration.test.js
@@ -1,4 +1,5 @@
 const supertest = require('supertest');
+const isValidDate = require('../../util/isValidDate');
 const mongoMemory = require('../../config/mongoMemory');
 const app = require('../../app');
 const Note = require('./DAL/model/Note');
@@ -9,11 +10,16 @@ const basePath = '/api/v1';
 
 const setup = () => { // equivalent to beforeEach, useful to prevent global variables
   
+  const basePath = '/api/v1';
+  const notesPath = `${basePath}/notes`;
+  const dummyNote = { content: 'note content' };
+  const dummyNoteList = new Array(12).fill(dummyNote);
+
   const populateDB = async (data) => { // data can be one Object or an Array of Objects
     await Note.create(data, { lean: true });
   }
 
-  return { populateDB }
+  return { notesPath, dummyNote, dummyNoteList, populateDB };
 }
 
 beforeAll(async () => {
@@ -29,20 +35,66 @@ afterAll(async () => {
   await mongoMemory.closeConnection();
 });
 
+
+describe('POST /notes', () => {
+  test('When creates a new note, then should return new note record as a JSON with status code 201', async () => {
+    const { dummyNote, notesPath } = setup();
+    
+    const response = await request
+      .post(notesPath)
+      .send(dummyNote);
+
+    const { status, headers, body } = response;
+    
+    expect(status).toBe(201);
+    expect(headers['content-type']).toContain('application/json');
+    expect(body).toEqual({
+      id: expect.any(String),
+      content: dummyNote.content,
+      isDone: false,
+      createdAt: expect.any(String)
+    });
+    expect(isValidDate(body.createdAt)).toBeTruthy();
+  });
+
+  test('When missing "conent" field, then should return error message as a JSON with status code 400', async () => {
+    const { notesPath } = setup();
+
+    const response = await request
+      .post(notesPath)
+      .send({});
+
+    const { status, headers, body } = response;
+
+    expect(status).toBe(400);
+    expect(headers['content-type']).toContain('application/json');
+    expect(body).toEqual({ message: 'Required key(s): content' });
+  });
+
+  test('When unhandled error occurs, then should return an error message as a JSON with status code 500', async () => {
+    jest //this is an stub with jest using spyOn
+      .spyOn(notesBLL, 'create')
+      .mockRejectedValue(new Error('random error'));
+    
+    const response = await request.post(`${basePath}/notes`);
+    const { status, headers, body } = response;
+    
+    expect(status).toBe(500);
+    expect(headers['content-type']).toContain('application/json');
+    expect(body).toEqual({ message: 'Server Error' });
+  });
+});
+
 describe('GET /notes', () => {
-  test('When request all notes, then should retrieves 10 notes at most by default as a JSON with status code 200', async () => {
-    // ARRANGA
+  test('When request all notes, then should return 10 notes at most by default as a JSON with status code 200', async () => {
     const { populateDB } = setup();
     const dummyList = new Array(12).fill({
       content: 'note content'
     });
     await populateDB(dummyList);
 
-
-    // ACT
     const response = await request.get(`${basePath}/notes`);
 
-    //ASSERT
     expect(response.status).toBe(200);
     expect(response.headers['content-type']).toContain('application/json');
     expect(response.body.length).toBeLessThanOrEqual(10);
@@ -57,14 +109,14 @@ describe('GET /notes', () => {
     });
   });
   
-  test('When request only 5 notes, then should retrieves 4 notes as a JSON with status code 200', async () => {
-    const { populateDB } = setup();
+  test('When request only 5 notes, then should return 4 notes as a JSON with status code 200', async () => {
+    const { populateDB, notesPath } = setup();
     const dummyList = new Array(12).fill({
       content: 'note content'
     });    
     await populateDB(dummyList);
 
-    const response = await request.get(`${basePath}/notes?limit=5`);
+    const response = await request.get(`${notesPath}?limit=5`);
 
     expect(response.status).toBe(200);
     expect(response.headers['content-type']).toContain('application/json');
@@ -80,7 +132,9 @@ describe('GET /notes', () => {
     });
   });
 
-  test('When unhandled error occurs, then should retrieves an error message as a JSON with status code 500', async () => {
+  test.todo('When recieve not valid "limit" param, then should retireves a bad request message as a JSON with status code 400');
+
+  test('When unhandled error occurs, then should return an error message as a JSON with status code 500', async () => {
     jest
       .spyOn(notesBLL, 'readAll')
       .mockRejectedValue(new Error('random error'));
@@ -91,6 +145,10 @@ describe('GET /notes', () => {
     expect(response.headers['content-type']).toContain('application/json');
     expect(response.body).toEqual({ message: 'Server Error' });
   });
-
-  test.todo('When recieve not valid "limit" param, then should retireves a bad request message as a JSON with status code 400');
 });
+
+// describe('GET /notes/:noteId', () => {});
+
+// describe('PATCH /notes/:noteId', () => {});
+
+// describe('DELETE /notes/:noteId', () => {});
