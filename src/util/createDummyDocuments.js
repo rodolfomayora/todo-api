1d1604a3811ec2ddd4ed7be6c35e77036ed6fa35
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-03-13 18:25:21 -0400

    feat: add dummy mongodb documents generatons for local development

---
+++ src/util/createDummyDocuments.js
@@ -0,0 +1,26 @@
+const { faker } = require('@faker-js/faker/locale/en');
+const Note = require('../apiServices/notes/DAL/model/Note');
+const logger = require('./logger');
+
+const createDummyDocuments = async () => {
+  const createNote = () => {
+    // const content = faker.random.words(4)
+    const content = faker.lorem.words(4)
+    return { content };
+  }
+
+  const createNoteList = (quantity) => {
+    return new Array(quantity)
+      .fill()
+      .map(createNote);
+  }
+
+  const saveNotes = async (data) => {
+    await Note.create(data);
+  }
+
+  await saveNotes(createNoteList(15));
+  return logger.info('Database populated with dummy documents');
+}
+
+module.exports = createDummyDocuments;
